7b032df7720098735b20723934aca74e05b5c133
NotThatNinja <samuele8max@gmail.com>
2025-03-20 19:41:11 +0100

    Finished backend + frontend fetching and submission!!!

--- backend/src/get.php
+++ backend/src/get.php
@@ -1,4 +1,9 @@
 <?php
+// Enable CORS TODO: Remove this in production
+header("Access-Control-Allow-Origin: http://localhost:4321");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
 header('Content-Type: application/json; charset=utf-8');
 
 if (isset($_GET['what'])) {

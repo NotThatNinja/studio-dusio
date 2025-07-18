744f753426ae6f2062f1ef86c644eea7a6f91dbb
NotThatNinja <samuele8max@gmail.com>
2025-07-18 23:36:34 +0200

    Remove base and github pages

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -5,8 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://notthatninja.github.io',
-  base: 'studio-dusio',
   vite: {
     plugins: [tailwindcss()]
   },

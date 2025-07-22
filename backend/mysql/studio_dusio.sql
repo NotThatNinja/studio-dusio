d540edd71a7e45949afb544d5eb2889f24fa4355
NotThatNinja <samuele8max@gmail.com>
2025-07-22 13:42:46 +0200

    Update db collation

--- backend/mysql/studio_dusio.sql
+++ backend/mysql/studio_dusio.sql
@@ -33,7 +33,7 @@ CREATE TABLE `alerts` (
   `title` varchar(255) NOT NULL,
   `text` text NOT NULL,
   `featured` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -48,7 +48,7 @@ CREATE TABLE `reviews` (
   `surname` varchar(255) NOT NULL,
   `text` text NOT NULL,
   `approved` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Indexes for dumped tables
